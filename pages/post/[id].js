9798b9f46dbeeb9a3011f706bd49d02eac1d4441
Charlie Villarino <charlievillarino@gmail.com>
2020-04-29 21:44:55 +0800

    Checking query id

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,27 +1,26 @@
 import { useRouter } from "next/router"
-import Link from "next/link"
 import Posts from "../../data/posts.json"
 import Layout from "../../components/Layout/index.js"
 import { Container, Row, Col, Alert } from "react-bootstrap"
 
 const PostID = () => {
   const { query } = useRouter()
-
-  let PostItem = []
-  if (query.id) {
-    for (let i = 0; i < Posts.length; i += 1) {
-      if (Posts[i].id === parseInt(query.id)) {
-        PostItem.push(Posts[i])
-      }
-    }
-  }
+  console.log(query.id)
+  // let PostItem = []
+  // if (query.id) {
+  //   for (let i = 0; i < Posts.length; i += 1) {
+  //     if (Posts[i].id === parseInt(query.id)) {
+  //       PostItem.push(Posts[i])
+  //     }
+  //   }
+  // }
 
   return (
     <Layout>
       <Container>
         <Row>
           <Col>
-            {" "}
+            {/* {" "}
             {PostItem.map((item, key) => {
               return (
                 <div key={key}>
@@ -29,7 +28,7 @@ const PostID = () => {
                   <Alert variant={"secondary"}>Title: {item.title}</Alert>
                 </div>
               )
-            })}
+            })} */}
           </Col>
         </Row>
       </Container>
